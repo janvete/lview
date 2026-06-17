48824dc6817f008e90a34dcafad95fc2ec1fe234
SW <janvete@gmail.com>
2026-06-17 21:34:42 +0200

    Add Homebrew formula

---
+++ Formula/lview.rb
@@ -0,0 +1,18 @@
+class Lview < Formula
+  desc "TUI for viewing remote logs over SSH"
+  homepage "https://github.com/janvete/lview"
+  url "https://github.com/janvete/lview/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "22ef0ace269a512afeeddfba27484656aafe61ac688c5a57e99bf6109277de28"
+  license "MIT"
+  head "https://github.com/janvete/lview.git", branch: "master"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match "TUI for viewing remote logs over SSH", shell_output("#{bin}/lview --help")
+  end
+end
