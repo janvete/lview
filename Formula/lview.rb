9996616d9bce4e198dedb7491fa915f6f79998db
SW <janvete@gmail.com>
2026-06-17 21:56:28 +0200

    Update formula to 0.1.3

--- Formula/lview.rb
+++ Formula/lview.rb
@@ -1,8 +1,8 @@
 class Lview < Formula
   desc "TUI for viewing remote logs over SSH"
   homepage "https://github.com/janvete/lview"
-  url "https://github.com/janvete/lview/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "6be5d34782878d4065bd5871f7995e0a67168f9ab003da89c91e73c7d9ac6a43"
+  url "https://github.com/janvete/lview/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "364787dc5ff1c81bdbc16d4cf3ebfa716f9cfa130357c02cfda2d20a12c3efd0"
   license "MIT"
   head "https://github.com/janvete/lview.git", branch: "master"
 
